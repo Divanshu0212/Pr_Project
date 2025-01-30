f502b918ce65c47ab5c736affc7389779c484e66
Divanshu Bhargava <divanshu0212@gmail.com>
2025-01-30 15:13:51 +0530

    Tech stack

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -81,7 +81,7 @@ const HomePage = ({ user }) => {
             </button>
             <button
               className="border border-[#00FFFF] px-8 py-3 text-xl rounded-lg hover:bg-[#161B22] transition-all text-[#00FFFF]"
-              onClick={() => navigate("/ats-tracker")}
+              onClick={() => navigate("/ats")}
             >
               Track ATS Scores
             </button>
